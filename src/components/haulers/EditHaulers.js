751409fd1fa77340cd707b4ae0838f2ee19724e5
Justin Miller <jrmill94@icloud.com>
2023-10-31 11:50:06 -0500

    modified edithaulers

--- src/components/haulers/EditHaulers.js
+++ src/components/haulers/EditHaulers.js
@@ -1,16 +1,24 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getHaulerById } from "../../Services/HaulerService";
+import { getDocks } from "../../Services/DockService";
 
 export const EditHaulers = () => {
   const { haulerId } = useParams();
-  const [hauler, setHauler] = useState({ name: "" });
+  const [hauler, setHauler] = useState({ name: "", dockId: 0 });
+  const [docks, setDocks] = useState([]);
 
   useEffect(() => {
     getHaulerById(haulerId).then((data) => {
       setHauler(data);
     });
   }, [haulerId]);
+
+  useEffect(() => {
+    getDocks().then((docks) => {
+      setDocks(docks);
+    });
+  }, []);
 
   const handleEditChange = (event) => {
     const { name, value } = event.target;
@@ -20,30 +28,40 @@ export const EditHaulers = () => {
     }));
   };
 
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <header>
           <h1>Edit Hauler</h1>
         </header>
         <fieldset>
           <input
-            name="title"
+            name="name"
             placeholder="hauler title"
             value={hauler.name || ""}
             type="text"
             onChange={handleEditChange}
-          ></input>
+          />
         </fieldset>
         <fieldset>
           <select
-            name="dock-name"
+            name="dockId"
             onChange={handleEditChange}
-            value={hauler.dock_id}
+            value={hauler.dockId}
           >
-            <option value={0}></option>
+            <option value={0}>Please select a dock</option>
+            {docks.map((dock) => (
+              <option key={dock.id} value={dock.id}>
+                {dock.location}
+              </option>
+            ))}
           </select>
         </fieldset>
+        <button type="submit">Update</button>
       </form>
     </>
   );
